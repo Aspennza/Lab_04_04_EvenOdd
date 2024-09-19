06a71dd23a07055b6d9c90b887a1823dd52b5e92
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-19 13:32:36 -0400

    All tasks complete

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        int numToExamine = 245;
+        int moduloResult = 0;
+
+        moduloResult = numToExamine % 2;
+
+        System.out.println("If the result of the modulo is 0, the value is even, and, if the result of the modulo is not 0, the value is odd. Because the remainder of 245 / 2 is " + moduloResult + ", " + numToExamine + " is an odd number.");
     }
 }
